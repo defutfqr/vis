c501761735cb790c4895f2059c0565096e69b011
defutfqr <gomboev13@bk.ru>
2025-10-26 21:55:29 +0700

    Create movable.kt

---
+++ movable.kt
@@ -0,0 +1,11 @@
+import kotlin.math.cos
+import kotlin.math.sin
+
+interface Movable {
+    val x: Double
+    val y: Double
+    val speed: Double
+    
+    fun move()
+    fun getPosition(): Pair<Double, Double> = Pair(x, y)
+}
